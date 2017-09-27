e87a4f7528d272cbc313f2fac3405f8827109b61
Niklas Schlüter <schlueter.niklas@gmail.com>
2017-09-27 14:59:19 +0200

    Update spectragram.js

--- spectragram.js
+++ spectragram.js
@@ -216,8 +216,8 @@ if ( typeof Object.create !== "function" ) {
 
 	// Instagram Access Data
 	jQuery.fn.spectragram.accessData = {
-        accessToken: null,
-		clientID: null
+        accessToken: 4208728467.1677ed0.b3af60b394a54b62a714b7a233de89b0,
+		clientID: e23801b6e54847fbb0273c9e01cb0e6e
     };
 
 } )( jQuery, window, document );
